ea5c969890c014ce355d960cb6f21fcec0a89db6
Angel <angelCL0@outlook.com>
2020-09-05 21:59:54 -0400

    Hecho

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.group}>
-        <h1>Un formulario de contacto pequeño pero bien hecho_</h1>
+        <h1>Un formulario de contacto_</h1>
         <ContactForm />
 
         <p>
