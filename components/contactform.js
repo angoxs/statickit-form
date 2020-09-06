955acb550bd2dd3e61ebda0f025363599691c255
Angel <angelCL0@outlook.com>
2020-09-05 23:04:53 -0400

    Final

--- components/contactform.js
+++ components/contactform.js
@@ -1,27 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm, ValidationError } from "@statickit/react";
 import styles from "./form.module.css";
 
-function ContactForm(value) {
+function ContactForm({ value }) {
   const [state, handleSubmit] = useForm("contactForm");
 
-  function validateEmail(value) {
-    let error;
-    if (!value) {
-      error = "Required";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-      error = "Invalid email address";
-    }
-    return error;
-  }
-
-  function validateUsername(value) {
-    let error;
-    if (value === "jdllf") {
-      error = "Nice try!";
-    }
-    return error;
-  }
   if (state.succeeded) {
     return <p className={styles.success}>Thanks for joining!</p>;
   }
@@ -32,7 +15,6 @@ function ContactForm(value) {
         id="name"
         type="name"
         name="name"
-        validate={validateUsername}
         className={styles.input}
         required
         placeholder="Enter your name"
@@ -42,7 +24,6 @@ function ContactForm(value) {
         id="email"
         type="email"
         name="email"
-        validate={validateEmail}
         className={styles.input}
         required
         placeholder="Enter your email"
